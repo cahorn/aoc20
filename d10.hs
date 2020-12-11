64580a29243f965a62e7716cac5bdfcfe69ce9e5
C. Ansel Horn <ansel@horn.name>
2020-12-11 16:57:27 -0500

    Simplify recursive list def in day 10 solution

--- d10.hs
+++ d10.hs
@@ -6,9 +6,9 @@ import AoC20Lib
 differences l = zipWith subtract l (tail l)
 
 arrangements as = arrs
-    where arrs = zipWith4 acc mask (1:arrs) (0:0:arrs) (0:0:0:arrs)
+    where arrs = 1 : zipWith4 acc mask arrs (0:arrs) (0:0:arrs)
           acc b x y z = b * (sum [x, y, z])
-          mask = [if elem i as then 1 else 0 | i <- [0..maximum as]]
+          mask = [if elem i as then 1 else 0 | i <- [1..maximum as]]
 
 adapters = (\as -> 0 : as ++ [maximum as + 3]) . sort . map read . lines
 
